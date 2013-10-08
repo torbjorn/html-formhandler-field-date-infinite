d6805cdbdc43d604ce81e20f6ee546c9a21a8801
Torbjørn Lindahl <torbjorn.lindahl@gmail.com>
2013-10-08 14:35:16 +0200

    upped version

--- lib/HTML/FormHandler/Field/Date/Infinite.pm
+++ lib/HTML/FormHandler/Field/Date/Infinite.pm
@@ -3,7 +3,7 @@ package HTML::FormHandler::Field::Date::Infinite;
 use Moose;
 extends 'HTML::FormHandler::Field::Date';
 
-use version; our $VERSION = qv('0.0.1');
+use version; our $VERSION = qv('0.1.0');
 
 override 'date_deflate' => sub {
 
