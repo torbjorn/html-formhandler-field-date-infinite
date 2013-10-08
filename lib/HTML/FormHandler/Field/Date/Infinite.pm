d9e66f1f89243e31205d29eb61013ea2d543b866
Torbjørn Lindahl <torbjorn.lindahl@gmail.com>
2013-10-08 14:30:03 +0200

    pod

--- lib/HTML/FormHandler/Field/Date/Infinite.pm
+++ lib/HTML/FormHandler/Field/Date/Infinite.pm
@@ -57,96 +57,40 @@ This document describes HTML::FormHandler::Field::Date::Infinite version 0.0.1
 
 =head1 SYNOPSIS
 
-    use HTML::FormHandler::Field::Date::Infinite;
+## In your form:
 
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
+has_field my_date => (
+    type => 'Date::Infinite',
+);
 
+## when rendered it will now display -inf / inf and accepts variations
+## of "infinite" as input and will update the model as
+## apropriate. Otherwise works as a normal HF::Field::Date
 
 =head1 DESCRIPTION
 
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
-
-
-=head1 INTERFACE
-
-=head2 date_deflate
-
-A default deflate method for HFH::Field::Date objects that correctly
-presents DT::Infinite objects.
+A slight alteration of the original FH::Field::Date to make it also
+accept infinite input for dates (DateTime supports this).
 
 =head1 DIAGNOSTICS
 
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
+This module does not introduce new errors, if any, they originate from
+in HTML::FormHandler::Field::Date or above.
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-
 HTML::FormHandler::Field::Date::Infinite requires no configuration files or environment variables.
-
 
 =head1 DEPENDENCIES
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
+DateTime
 
 =head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
 
 None reported.
 
 
 =head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
 
 No bugs have been reported.
 
@@ -154,11 +98,9 @@ Please report any bugs or feature requests to
 C<bug-html-formhandler-field-date-infinite@rt.cpan.org>, or through the web interface at
 L<http://rt.cpan.org>.
 
-
 =head1 AUTHOR
 
 Torbjørn Lindahl  C<< <torbjorn.lindahl@gmail.com> >>
-
 
 =head1 LICENCE AND COPYRIGHT
 
